8c7734f4fc71b958b8b8a84c57a60b0d4cc0c99a
Park Hyeon Chae <park19996@naver.com>
2019-08-28 16:55:02 +0900

    이미지 선택 구현

--- app.py
+++ app.py
@@ -13,12 +13,15 @@ def index():
         res = requests.post(url)
         html = res.content
         soup = BeautifulSoup(html, "html.parser")
-
+        
         img_names = soup.find_all('img')
 
+        count = 0
         imgdata = []
         for img in img_names:
             imgdata.append(img['src'])
+            count+=1
+            if count == 16: break
 
         return render_template('index.html', imglist=imgdata)
     else:
@@ -64,3 +67,34 @@ if __name__ == "__main__":
 
 
 # print (imgdata)
+
+
+# from flask import Flask, render_template, request, url_for, redirect
+# from bs4 import BeautifulSoup
+# import requests
+
+# app = Flask(__name__) 
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         keyword = request.form['keyword']
+#         url = "https://unsplash.com/search/photos/"+keyword
+        
+#         res = requests.post(url)
+#         html = res.content
+#         soup = BeautifulSoup(html, "html.parser")
+
+#         divtag = soup.find('div',{'class' : 'IEpfq'})
+#         #img_names = divtag.find('img')
+
+#         # imgdata = []
+#         # for img in img_names:
+#         #     imgdata.append(img['src'])
+
+#         return render_template('index.html', imglist=divtag)
+#     else:
+#         return render_template('index.html')
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
