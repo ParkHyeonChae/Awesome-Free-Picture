dd2fef68c4b008c21397758cd1125fc75df74eeb
ParkHyeonChae <park19996@naver.com>
2019-08-29 20:34:52 +0900

    javascript로 선택이미지 push 구현

--- app.py
+++ app.py
@@ -27,81 +27,10 @@ def index():
     else:
         return render_template('index.html')
 
-@app.route('/test')
-def addimg(imglist):
-    if imglist is not None:
-        imgurls = []
-        imgurls.append(imglist)
-        return render_template('test.html', data=imgurls)
-    else:
-        return render_template('test.html', data=imglist)
+# def imgsave(imgurl):
+#     imgurllist = []
+#     imgurllist.append(imgurl)
+#     return imgurllist
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# from bs4 import BeautifulSoup
-# import requests
-
-# keyword = input("검색 이미지 : ")
-# url = "https://pixabay.com/images/search/"+keyword
-
-# res = requests.post(url)
-# html = res.content
-# soup = BeautifulSoup(html, "html.parser")
-
-# img_names = soup.find_all('img')
-
-# for img in img_names:
-#     imgdata = img['src']
-#     print (imgdata)
-
-
-# from bs4 import BeautifulSoup
-# import requests
-
-# keyword = input("검색 이미지 : ")
-# url = "https://pixabay.com/images/search/"+keyword
-
-# res = requests.post(url)
-# html = res.content
-# soup = BeautifulSoup(html, "html.parser")
-
-# img_names = soup.find_all('img')
-
-# imgdata = []
-# for img in img_names:
-#     imgdata.append(img['src'])
-
-
-# print (imgdata)
-
-
-# from flask import Flask, render_template, request, url_for, redirect
-# from bs4 import BeautifulSoup
-# import requests
-
-# app = Flask(__name__) 
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         keyword = request.form['keyword']
-#         url = "https://unsplash.com/search/photos/"+keyword
-        
-#         res = requests.post(url)
-#         html = res.content
-#         soup = BeautifulSoup(html, "html.parser")
-
-#         divtag = soup.find('div',{'class' : 'IEpfq'})
-#         #img_names = divtag.find('img')
-
-#         # imgdata = []
-#         # for img in img_names:
-#         #     imgdata.append(img['src'])
-
-#         return render_template('index.html', imglist=divtag)
-#     else:
-#         return render_template('index.html')
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
