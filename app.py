8b918d0bbe4cf20e68b069ab6544e1bf294ddd7a
ParkHyeonChae <park19996@naver.com>
2019-09-11 20:27:39 +0900

    함수화, mp4파일 크롤링

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app.secret_key = 'sample_secret'
 nowpage = 1
 a = 0
 b = 9
-imgdata = []
-viewdata = imgdata[a:b]
+data = []
+viewdata = data[a:b]
+viewtype = ''
 
 @app.route('/')
 def index():
@@ -17,101 +18,133 @@ def index():
 @app.route('/imgCrawling', methods=['POST'])
 def imgCrawling():
     if request.method == 'POST':
-        global imgdata, nowpage
-        imgdata = []
+        global data, nowpage, viewtype
+        viewtype = 'img'
+        data = []
         nowpage = 1
-        for page in range(1,5):
-            session['keyword'] = request.form['imgkeyword']
-            keyword = session['keyword']
-            url = "https://pixabay.com/images/search/"+keyword+"/?pagi=%d"%(page)
-            imgdata.extend(clawling(url))
+
+        for page in range(1,3):
+            keyword = keyword_()
+            url = 'https://pixabay.com/images/search/'+keyword+'/?pagi=%d'%(page)
+            soup = crawling(url)
+
+            img_names = soup.find_all('div', class_ = 'item')
+            for img in img_names:
+                if(img.find('img')['src'][0:4]=='http' and img.find('img')['src'][-3:]=='jpg'):
+                    data.append(img.find('a').find('img')['src'][:-8]+'1280.jpg')
+
+                elif(img.find('img')['src'][0:4]=='http' and img.find('img')['src'][-3:]=='png'):
+                    data.append(img.find('a').find('img')['src'][:-8]+'1280.png')
+
+                else:
+                    if(img.find('img')['data-lazy'][-3:]=='jpg'):
+                        data.append(img.find('a').find('img')['data-lazy'][:-8]+'1280.jpg')
+                    else:
+                        data.append(img.find('a').find('img')['data-lazy'][:-8]+'1280.png')
         
         viewlist()
-        print(viewdata)
-        return render_template('imglist.html', imglist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
+        
+        return render_template('imglist.html', viewlist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
     else:
         return render_template('imglist.html')
 
 @app.route('/iconCrawling', methods=['GET', 'POST'])
 def iconCrawling():
     if request.method == 'POST':
-        session['keyword'] = request.form['imgkeyword']
-        keyword = session['keyword']
-        url = "https://iconmonstr.com/?s="+keyword
-        res = requests.post(url)
-        html = res.content
-        soup = BeautifulSoup(html, "html.parser")
+        global viewtype
+        viewtype = 'img'
+        data = []
+        keyword = keyword_()
+        url = 'https://iconmonstr.com/?s='+keyword
+        soup = crawling(url)
 
-        img_names = soup.find_all('img', class_ = "preload")
-
-        imgdata = []
+        img_names = soup.find_all('img', class_ = 'preload')
         for img in img_names:
-            imgdata.append(img['src'])
+            data.append(img['src'])
 
-        return render_template('iconlist.html', imglist=imgdata, keyword=keyword, tmp=not None)
+        return render_template('iconlist.html', viewlist=data, keyword=keyword, tmp=not None)
     else:
         return render_template('iconlist.html')
 
-@app.route('/imgCrawling/nextpage', methods=['GET','POST'])
+@app.route('/videoCrawling', methods=['GET', 'POST'])
+def videoCrawling():
+    if request.method == 'POST':
+        global data, nowpage, viewtype
+        viewtype = 'video'
+        data = []
+        nowpage = 1
+        keyword = keyword_()
+        url = 'https://pixabay.com/ko/videos/search/'+keyword
+        soup = crawling(url)
+
+        img_names = soup.find_all('div', class_ = 'item')
+        for img in img_names:
+            data.append(img.find('div', class_ = 'media')['data-mp4'])
+        
+        viewlist()
+
+        return render_template('videolist.html', viewlist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
+    else:
+        return render_template('videolist.html')
+
+@app.route('/nextpage', methods=['GET','POST'])
 def nextpage():
     keyword = session['keyword']    
-    global nowpage
+    global nowpage, viewtype
+    
     nowpage += 1
 
     viewlist()
 
-    return render_template('imglist.html', imglist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
+    if viewtype == 'img':
+        return render_template('imglist.html', viewlist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
+    elif viewtype == 'video':
+        return render_template('videolist.html', viewlist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
 
-@app.route('/imgCrawling/backpage', methods=['POST','GET'])
+@app.route('/backpage', methods=['POST','GET'])
 def backpage():
     keyword = session['keyword'] 
-    global nowpage
+    global nowpage, viewtype
+
     nowpage -= 1
     if nowpage < 1:nowpage = 1
 
     viewlist()
 
-    return render_template('imglist.html', imglist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
+    if viewtype == 'img':
+        return render_template('imglist.html', viewlist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
+    elif viewtype == 'video':
+        return render_template('videolist.html', viewlist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
 
-@app.route('/imgCrawling/pagemove', methods=['POST'])
+@app.route('/pagemove', methods=['POST'])
 def pagemove():
     keyword = session['keyword'] 
-    global nowpage
+    global nowpage, viewtype
     nowpage = int(request.form['nowpagenum'])
     viewlist()
 
-    return render_template('imglist.html', imglist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage) 
+    if viewtype == 'img':
+        return render_template('imglist.html', viewlist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
+    elif viewtype == 'video':
+        return render_template('videolist.html', viewlist=viewdata, keyword=keyword, tmp=not None, pagenum=nowpage)
 
-def clawling(url):
+def crawling(url):
     res = requests.post(url)
     html = res.content
-    soup = BeautifulSoup(html, "html.parser")
-    img_names = soup.find_all("div", class_ = "item")
+    soup = BeautifulSoup(html, 'html.parser') 
+    return soup
 
-    imgdata = []
-    for img in img_names:
-        if(img.find('img')['src'][0:4]=='http' and img.find('img')['src'][-3:]=='jpg'):
-            imgdata.append(img.find('a').find('img')['src'][:-8]+"1280.jpg")
-
-        elif(img.find('img')['src'][0:4]=='http' and img.find('img')['src'][-3:]=='png'):
-            imgdata.append(img.find('a').find('img')['src'][:-8]+"1280.png")
-
-        else:
-            if(img.find('img')['data-lazy'][-3:]=='jpg'):
-                imgdata.append(img.find('a').find('img')['data-lazy'][:-8]+"1280.jpg")
-            else:
-                imgdata.append(img.find('a').find('img')['data-lazy'][:-8]+"1280.png")
-        
-    return imgdata
+def keyword_():
+    session['keyword'] = request.form['imgkeyword']
+    keyword = session['keyword']
+    return keyword
 
 def viewlist():
-    global a, b, nowpage, viewdata, imgdata
-
+    global a, b, nowpage, viewdata, data
     a = (int(nowpage)-1) * 9 
     b = int(nowpage) * 9
-    viewdata = imgdata[a:b]
-    
+    viewdata = data[a:b]
     return viewdata
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
